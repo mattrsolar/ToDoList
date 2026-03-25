6c8f390d759338a07143c32a2de4a6181869d84f
mattrsolar <mateusr.almeidas@gmail.com>
2026-03-25 19:55:21 +0800

    first commit

---
+++ ToDoList/src/App.tsx
@@ -0,0 +1,40 @@
+import Text from "./components/text";
+import Icon from "./components/icon";
+import TrashIcon from "./assets/icons/trash.svg?react"
+import SpinnerIcon from "./assets/icons/spinner.svg?react"
+import PlusIcon from "./assets/icons/plus.svg?react"
+import Badge from "./components/badge";
+import Button from "./components/button";
+
+
+function App() {
+
+  return (
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          HELLLO
+        </Text>
+        <Text variant="body-sm-bold" className="text-pink-base">
+
+        </Text> 
+
+      </div>
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base"/>
+        <Icon svg={SpinnerIcon} animate/>
+      </div>
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">5 de 10</Badge>
+
+      </div>
+      <div>
+        <Button icon={PlusIcon}>New Task</Button>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ ToDoList/src/components/button.tsx
@@ -0,0 +1,76 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import Icon from "./icon";
+import Text from "./text";
+
+export const buttonVariants = cva("flex item-center justify-center cursor-pointer transition rounded-lg group gap-2",{
+    variants:{
+        variant:{
+            primary: "bg-gray-200 hover:bg-pink-light"
+        },
+        size:{
+            md: "h-14 py-4 px-5"
+        },
+        disabled:{
+            true:"opacity-50 pointer-events-none"
+        }
+    },
+            defaultVariants:{
+            variant:"primary",
+            size:"md",
+            disabled: false
+        }
+});
+
+export const buttonIconVariants = cva("transition",{
+    variants:{
+        variant:{
+            primary:"fill-pink-base"
+        },
+        size:{
+            md:"w-5 h-5"
+        }
+    },
+    defaultVariants:{
+        variant: "primary",
+        size:"md"
+    }
+})
+
+export const buttonTextVariants = cva("",{
+    variants:{
+        variant:{
+            primary:"text-gray-400"
+        }
+    },
+    defaultVariants:{
+        variant: "primary",
+    }
+})
+
+
+interface ButtonProps extends Omit<React.ComponentProps<"button">, "size" | "disabled">, VariantProps<typeof buttonVariants>{
+    icon?: React.ComponentProps<typeof Icon>["svg"]
+}
+
+function Button({
+    variant,
+    size,
+    disabled,
+    className,
+    children,
+    icon: IconComponent,
+    ...props
+}:ButtonProps){
+
+    return (
+        <button className={buttonVariants({variant, size, disabled, className})} {...props}>
+            {IconComponent && <Icon className={buttonIconVariants({variant, size})} svg={IconComponent}/>}
+            <Text variant="body-md-bold" className={buttonTextVariants({variant})}>
+                {children}
+            </Text>
+        </button>
+    ) 
+}
+
+
+export default Button;
