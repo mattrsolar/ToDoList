c9ba7c815bf944f1c99669727cd119b1f673c8ea
mattrsolar <mateusr.almeidas@gmail.com>
2026-03-26 20:26:29 +0800

    Created basic routing

--- ToDoList/src/App.tsx
+++ ToDoList/src/App.tsx
@@ -1,68 +1,18 @@
-import Text from "./components/text";
-import Icon from "./components/icon";
-import TrashIcon from "./assets/icons/trash.svg?react"
-import SpinnerIcon from "./assets/icons/spinner.svg?react"
-import PlusIcon from "./assets/icons/plus.svg?react"
-import Badge from "./components/badge";
-import Button from "./components/button";
-import ButtonIcon from "./components/button-icon";
-import InputText from "./components/input-text";
-import InputCheckbox from "./components/input-checkbox";
-import Card from "./components/card";
-import Container from "./components/container";
-import Skeleton from "./components/skeleton";
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import PageComponent from "./pages/page-component";
+import LayoutMain from "./pages/layout-main";
+import PageHome from "./pages/page-home";
 
 function App() {
-
   return (
-    <Container>
-      <div className="grid gap-3">
-        <div className="flex flex-col gap-2">
-          <Text variant="body-sm-bold" className="text-pink-base">
-            HELLLO
-          </Text>
-          <Text variant="body-sm-bold" className="text-pink-base">
-
-          </Text> 
-
-        </div>
-        <div className="flex gap-1">
-          <Icon svg={TrashIcon} className="fill-green-base"/>
-          <Icon svg={SpinnerIcon} animate/>
-        </div>
-        <div className="flex gap-1">
-          <Badge variant="secondary">5</Badge>
-          <Badge variant="primary">5 de 10</Badge>
-          <Badge loading>Loading</Badge>
-
-        </div>
-        <div>
-          <Button icon={PlusIcon}>New Task</Button>
-        </div>
-        <div className="flex gap-1">
-          <ButtonIcon icon={TrashIcon} variant="primary"/>
-          <ButtonIcon icon={TrashIcon} loading/>
-        </div>
-        <div>
-          <InputText placeholder="Type your task here"/>
-        </div>
-        <div>
-          <InputCheckbox/>
-          <InputCheckbox loading/>
-        </div>
-        <div>
-          <Card size="md">
-            This is a card
-          </Card>
-        </div>
-
-        <div>
-          <Skeleton className="h-10"/>
-          <Skeleton className="w-10 h-10"/>
-        </div>
-      </div>    
-    </Container>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LayoutMain />}>
+          <Route index element={<PageHome />} />
+          <Route path="/components" element={<PageComponent />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
